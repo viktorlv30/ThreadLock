772adbad736b6839e80d97dfee67d5923c0c27a7
viktorlv30 <viktor.lv.30@gmail.com>
2016-09-22 12:24:38 +0300

    make two levels of thread lock

---
+++ ThreadLock/Account.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ThreadLock
+{
+
+    class Account
+    {
+
+        private readonly object _withdrawLock = new object();
+        private readonly object _addLock = new object();
+        private readonly object _devideLock = new object();
+
+        private int _balance;
+        private int _operationNumber = 0;
+
+        Random _rand = new Random();
+
+        public Account(int amount)
+        {
+            _balance = amount;
+        }
+
+        private int Withdraw(int amount)
+        {
+            Console.WriteLine("START withdraw operation");
+            // This condition never is true unless the lock statement
+            // is commented out.
+            if (_balance < 0)
+            {
+                throw new Exception("Negative Balance");
+            }
+
+            // Comment out the next line to see the effect of leaving out 
+            // the lock keyword.
+            lock (_devideLock)
+            {
+                lock (_withdrawLock)
+                {
+                    Console.WriteLine("==============Lock thread withdraw inside devide================");
+                    if (_balance >= amount)
+                    {
+                        Console.WriteLine($"Balance before Withdrawal {++_operationNumber}:  " + _balance);
+                        Console.WriteLine("Amount to Withdraw          : -" + amount);
+                        _balance -= amount;
+                        Console.WriteLine("Balance after Withdrawal    :  " + _balance);
+                        Console.WriteLine($"FINISH withdraw operation {_operationNumber}\n");
+                        return amount;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transaction withdraw rejected");
+                        return 0; // transaction rejected
+                    }
+                }
+            }
+            
+        }
+
+
+        private int Add(int amount)
+        {
+            Console.WriteLine("START Add operation");
+            // Comment out the next line to see the effect of leaving out 
+            // the lock keyword.
+            lock (_devideLock)
+            {
+                lock (_withdrawLock)
+                {
+                    Console.WriteLine("=================Lock thread add inside divide==================");
+                    if (_balance >= amount)
+                    {
+                        Console.WriteLine($"Balance before Add {++_operationNumber}:  " + _balance);
+                        Console.WriteLine("Amount to Add        :  " + amount);
+                        _balance += amount;
+                        Console.WriteLine("Balance after Add    :  " + _balance);
+                        Console.WriteLine($"FINISH Add operation {_operationNumber}\n");
+                        return amount;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transaction Add rejected");
+                        return 0; // transaction rejected
+                    }
+                }
+
+            }
+            
+        }
+
+
+        private int Devide(int amount)
+        {
+            Console.WriteLine("START devide operation");
+            // This condition never is true unless the lock statement
+            // is commented out.
+            if (_balance < 0 || _balance < amount)
+            {
+                throw new Exception("Negative Balance");
+            }
+
+            // Comment out the next line to see the effect of leaving out 
+            // the lock keyword.
+            lock (_devideLock)
+            {
+                Console.WriteLine("===============Lock thread devide=================");
+                if (_balance >= amount)
+                {
+                    Console.WriteLine($"Balance before devide {++_operationNumber}:  " + _balance);
+                    Console.WriteLine("Amount to devide        :  " + amount);
+                    _balance /= amount;
+                    Console.WriteLine("Balance after devide    :  " + _balance);
+                    Console.WriteLine($"FINISH devide operation {_operationNumber}\n");
+                    return amount;
+                }
+                else
+                {
+                    Console.WriteLine("Transaction devide rejected");
+                    return 0; // transaction rejected
+                }
+            }
+        }
+
+
+        public void DoWithdrawTransactions()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                Withdraw(_rand.Next(1, 10));
+            }
+        }
+
+        public void DoAddTransactions()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                Add(_rand.Next(1, 100));
+            }
+        }
+
+        public void DoDevideTransactions()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                Devide(_rand.Next(1, 3));
+            }
+        }
+
+
+    }
+
+}
--- ThreadLock/Program.cs
+++ ThreadLock/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadLock
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var threads = new Thread[9];
+            var acc = new Account(1000);
+
+            for (var i = 0; i < 3; i++)
+            {
+                var t = new Thread(new ThreadStart(acc.DoWithdrawTransactions));
+                threads[i*3] = t;
+                //Thread.Sleep(100);
+
+                t = new Thread(new ThreadStart(acc.DoDevideTransactions));
+                threads[i*3+1] = t;
+                //Thread.Sleep(100);
+
+                t = new Thread(new ThreadStart(acc.DoAddTransactions));
+                threads[i*3+2] = t;
+                //Thread.Sleep(100);
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                threads[i].Start();
+            }
+            Console.ReadLine();
         }
     }
 }
